e1673386144ef88adb463a548985d8b21bb24d65
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-02 23:00:06 -0600

    etiqueta ts

---
+++ etiqueta.js
@@ -0,0 +1,5 @@
+function impeti(etiqueta) {
+    console.log(etiqueta.label);
+}
+var mietiquet = { numero: 10, label: "esta es mi etiqueta" };
+impeti(mietiquet);
---
+++ etiqueta.ts
@@ -0,0 +1,5 @@
+function impeti(etiqueta: {label:string}){
+    console.log(etiqueta.label);
+}
+let mietiquet = {numero: 10, label: "esta es mi etiqueta"};
+impeti(mietiquet);
