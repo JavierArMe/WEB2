294b3bb73192ceec5e7dcb5aa78604fb895b3e5a
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-02 23:01:54 -0600

    etiquetas ts

--- etiqueta.js
+++ etiqueta.js
@@ -3,3 +3,8 @@ function impeti(etiqueta) {
 }
 var mietiquet = { numero: 10, label: "esta es mi etiqueta" };
 impeti(mietiquet);
+function impeti2(etiqueta2) {
+    console.log(etiqueta2.label);
+}
+var mietiquet2 = { numero: 10, label: "esta es mi etiqueta" };
+impeti(mietiquet2);
--- etiqueta.ts
+++ etiqueta.ts
@@ -3,3 +3,14 @@ function impeti(etiqueta: {label:string}){
 }
 let mietiquet = {numero: 10, label: "esta es mi etiqueta"};
 impeti(mietiquet);
+
+interface etiqueta2{
+    label: string;
+}
+
+
+function impeti2(etiqueta2: {label:string}){
+    console.log(etiqueta2.label);
+}
+let mietiquet2 = {numero: 10, label: "esta es mi etiqueta"};
+impeti(mietiquet2);
