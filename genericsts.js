c7565349943572b6daf6a55b564e80eda1822690
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 17:58:02 -0600

    generics normales

---
+++ genericsts.js
@@ -0,0 +1,6 @@
+var cajadestring = { contenido: "hola mundo" };
+var cajadenumero = { contenido: 100 };
+var cajadefecha = { contenido: new Date() };
+var cajitadestring = { contenido: "hola mundo" };
+var cajitadenumero = { contenido: 100 };
+var cajitadefecha = { contenido: new Date() };
