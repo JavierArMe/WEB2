6d70a4c6000a8de5afe9d2184d7842191b04ad2c
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 16:16:15 -0600

    parametrosopcionales

---
+++ parametrosopcionales.js
@@ -0,0 +1,8 @@
+function f(n) {
+    console.log(n.toFixed());
+    console.log(n.toFixed(3));
+}
+function f2(n2) {
+    console.log(n2 === null || n2 === void 0 ? void 0 : n2.toFixed());
+    console.log(n2 === null || n2 === void 0 ? void 0 : n2.toFixed(3));
+}
