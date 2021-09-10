5fad97f432d5c4834df687531387d594fbd473ae
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 16:29:27 -0600

    funcionesthis

---
+++ funcionesthis.js
@@ -0,0 +1,20 @@
+var usuario = {
+    id: 123,
+    admin: false,
+    volverseradmin: function () {
+        this.admin = true;
+    }
+};
+console.log(usuario.admin);
+usuario.volverseradmin();
+console.log(usuario.admin);
+var usuario2 = {
+    id: 123,
+    admin: false,
+    volverseradmin2: function () {
+        this.admin = true;
+    }
+};
+console.log(usuario.admin);
+usuario2.volverseradmin2();
+console.log(usuario.admin);
