da771eee9fb027549c5bb0a49ee3aba236c2ed02
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 16:26:10 -0600

    funciones overload

---
+++ funcionesoverload.js
@@ -0,0 +1,10 @@
+function longitud(x) {
+    return x.length;
+}
+console.log(longitud("hola mundo"));
+console.log(longitud([1, 2, 3, 4, 5]));
+function calcularlongitud(x) {
+    return x.length;
+}
+console.log(calcularlongitud("hola mundo"));
+console.log(calcularlongitud([1, 2, 3, 4, 5]));
