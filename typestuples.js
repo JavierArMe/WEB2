c47383b9392b068785583f475b4f7c790d3fa538
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 18:12:25 -0600

    typetuples

---
+++ typestuples.js
@@ -0,0 +1,8 @@
+var prius = ['toyota', 2015];
+var civic = ['honda', 2016];
+console.log('el prius es marca', prius[0], 'y modelo', prius[1]);
+console.log('el civic es marca', civic[0], 'y modelo', civic[1]);
+var prius2 = ['toyota', 2015];
+var marca = prius[0], modelo = prius[1];
+console.log("la marca del prius es:", marca);
+console.log("el modelo del prius es:", modelo);
