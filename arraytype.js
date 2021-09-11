c8979ebe001a6239e04a8a96d59324afaaef9669
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-10 18:00:51 -0600

    aarraytype

---
+++ arraytype.js
@@ -0,0 +1,11 @@
+var imprimirtareas = function (v) {
+    v.forEach(function (v) {
+        console.log(v);
+    });
+};
+var mistareas = [
+    "levantarse",
+    "lavarse los dientes",
+    "sacar al perro",
+];
+imprimirtareas(mistareas);
