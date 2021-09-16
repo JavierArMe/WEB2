9b7ab4571d73415ffa8a8e8861833757edca85ee
JavierArMe <franciscoaraujomeza@gmail.com>
2021-09-16 14:03:31 -0600

    classes herencia

---
+++ ClassesHerencia.js
@@ -0,0 +1,8 @@
+var television = /** @class */ (function () {
+    function television() {
+    }
+    television.prototype.encender = function () {
+        console.log("el televisor se ha encendido");
+    };
+    return television;
+}());
